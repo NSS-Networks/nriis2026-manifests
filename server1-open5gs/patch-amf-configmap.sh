bbace2e0a4f114a3edec308ab7209bd5ab314229
NSS-Network <s.natchars@gmail.com>
2026-05-19 16:16:36 +0700

    fix: AMF use NRF directly instead of SCP

--- server1-open5gs/patch-amf-configmap.sh
+++ server1-open5gs/patch-amf-configmap.sh
@@ -8,7 +8,7 @@ set -e
 
 echo "[1/3] Patching AMF ConfigMap..."
 kubectl patch configmap -n open5gs open5gs-amf --type=json -p='[
-  {"op": "replace", "path": "/data/amf.yaml", "value": "logger:\n  level: info\n\nglobal:\n\namf:\n  sbi:\n    server:\n    - address: 10.162.0.1\n      port: 7777\n    client:\n      scp:\n      - uri: http://open5gs-scp-sbi:7777\n  ngap:\n    server:\n    - address: 10.162.0.1\n  guami:\n    - amf_id:\n        region: 2\n        set: 1\n      plmn_id:\n        mcc: \"001\"\n        mnc: \"01\"\n  tai:\n    - plmn_id:\n        mcc: \"001\"\n        mnc: \"01\"\n      tac:\n      - 1\n  plmn_support:\n    - plmn_id:\n        mcc: \"001\"\n        mnc: \"01\"\n      s_nssai:\n      - sst: 1\n  security:\n    integrity_order: [NIA2, NIA1, NIA0]\n    ciphering_order: [NEA0, NEA1, NEA2]\n  network_name:\n    full: Gradiant\n  amf_name: open5gs-amf\n  time:\n    t3512:\n      value: 540\n"}
+  {"op": "replace", "path": "/data/amf.yaml", "value": "logger:\n  level: info\n\nglobal:\n\namf:\n  sbi:\n    server:\n    - address: 10.162.0.1\n      port: 7777\n    client:\n      nrf:\n      - uri: http://open5gs-nrf-sbi:7777\n  ngap:\n    server:\n    - address: 10.162.0.1\n  guami:\n    - amf_id:\n        region: 2\n        set: 1\n      plmn_id:\n        mcc: \"001\"\n        mnc: \"01\"\n  tai:\n    - plmn_id:\n        mcc: \"001\"\n        mnc: \"01\"\n      tac:\n      - 1\n  plmn_support:\n    - plmn_id:\n        mcc: \"001\"\n        mnc: \"01\"\n      s_nssai:\n      - sst: 1\n  security:\n    integrity_order: [NIA2, NIA1, NIA0]\n    ciphering_order: [NEA0, NEA1, NEA2]\n  network_name:\n    full: Gradiant\n  amf_name: open5gs-amf\n  time:\n    t3512:\n      value: 540\n"}
 ]'
 
 echo "[2/3] Restarting AMF deployment..."
